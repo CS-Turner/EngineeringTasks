71e579d76084b6f345069c7036f0a59e7b7314d2
MathFac2DS <157917925+MathFac2DS@users.noreply.github.com>
2026-03-24 10:54:18 -0400

    leetcode arithmeticandbasicreasoning.py tweaks to optimize, Q1 lots of trade-offs between mem and runtime

--- LeetCode/arithmeticandbasicreasoning.py
+++ LeetCode/arithmeticandbasicreasoning.py
@@ -12,14 +12,43 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
+        length = len(arr)
+        if length < 2: return True
 
-        arr.sort()
+        maximum = max(arr)
+        minimum = min(arr)
 
-        difference = arr[1] - arr[0]
-        for i in range(2, len(arr)):
-            if arr[i] - arr[i - 1] != difference:
-                return False
+        # diff = 0, true
+        if minimum == maximum: return True
+
+        # difference can't be found (integer)
+        if (maximum - minimum) % (length - 1) != 0: return False
+
+        difference = (maximum - minimum) // (length - 1)
+
+        i = 0
+        while i < length:
+            if arr[i] == minimum + i * difference: # in the correct position, and arithmetic
+                i += 1
+            else:
+                if (arr[i] - minimum) % difference != 0: # observed not arithmetic
+                    return False
+                position = (arr[i] - minimum) // difference # incorrect position, where should it be
+                if arr[position] == arr[i]: return False # duplicate value in array
+                else:
+                    arr[position], arr[i] = arr[i], arr[position] # move it to the correct position
         return True
+
+
+
+
+        # arr.sort()
+        #
+        # difference = arr[1] - arr[0]
+        # for i in range(2, len(arr)):
+        #     if arr[i] - arr[i - 1] != difference:
+        #         return False
+        # return True
 
 
 
